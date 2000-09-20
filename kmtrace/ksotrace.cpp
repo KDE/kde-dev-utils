fae753e5af3b5fa28d9d00155ea0ab6f136edabc
Waldo Bastian <bastian@kde.org>
2000-09-20 19:12:52 +0000

    Exclude getpwuid, unset LD_PRELOAD
    svn path=/trunk/kdesdk/kmtrace/; revision=64517

--- kmtrace/ksotrace.cpp
+++ kmtrace/ksotrace.cpp
@@ -1,9 +1,10 @@
 #include "ktrace.h"
+#include <stdlib.h>
 
 class KTraceActivate 
 {
 public:
-   KTraceActivate() { ktrace(); }
+   KTraceActivate() { setenv("LD_PRELOAD","",1); ktrace(); }
    ~KTraceActivate() { kuntrace(); }
 } kTraceActivateInstance;
 
