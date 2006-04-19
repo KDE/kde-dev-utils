535c9e39290b6469422ed865da8e9c7da018099f
Benjamin Meyer <ben@meyerhome.net>
2006-04-19 16:52:59 +0000

    forward port fix
    svn path=/trunk/KDE/kdesdk/kuiviewer/; revision=531624

--- kuiviewer/kuiviewer.cpp
+++ kuiviewer/kuiviewer.cpp
@@ -150,6 +150,7 @@ void KUIViewer::fileOpen()
 void KUIViewer::takeScreenshot(const Q3CString &filename, int w, int h){
     if(!m_part)
         return;
+    showMinimized();
     if(w!=-1 && h!=-1){
         // resize widget to the desired size
         m_part->widget()->setMinimumSize(w, h);
