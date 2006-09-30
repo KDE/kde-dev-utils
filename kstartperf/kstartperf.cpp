b2561fb865497ab20985033a81727b2b23259a57
Thomas Häber <thomas@haeber.de>
2006-09-30 21:09:25 +0000

    replaced assignment to QString::null with preferred QString() (EBN)
    svn path=/trunk/KDE/kdesdk/kstartperf/; revision=590792

--- kstartperf/kstartperf.cpp
+++ kstartperf/kstartperf.cpp
@@ -35,7 +35,7 @@ static KCmdLineOptions options[] =
 
 QString libkstartperf()
 {
-    QString lib = QString::null;
+    QString lib = QString();
     QString la_file = KStandardDirs::locate("lib", "libkstartperf.la");
 
     if (la_file.isEmpty())
