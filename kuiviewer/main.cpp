a12f713b05dd14e907a08629fcec0e12ac44372a
Friedrich W. H. Kossebau <kossebau@kde.org>
2021-08-14 16:42:22 +0200

    Port away from deprecated RESTORE macro
    GIT_SILENT

--- kuiviewer/main.cpp
+++ kuiviewer/main.cpp
@@ -59,7 +59,7 @@ int main(int argc, char** argv)
 
     // see if we are starting with session management
     if (app.isSessionRestored()) {
-        RESTORE(KUIViewer)
+        kRestoreMainWindows<KUIViewer>();
     } else {
         // no session.. just start up normally
 
